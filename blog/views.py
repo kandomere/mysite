2d922a70ad5d831b5d9497b089edfc50b4c1b911
kandomere <assasin21rus@gmail.com>
2021-01-04 01:34:19 +0300

    работает пагинация

---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import Post
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(status='published')
+    paginator = Paginator(posts, 2)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts ': posts, 'page': page})
+
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
