d2116e34b0eb93d25842342f2cba82d9f3f4ca56
kandomere <assasin21rus@gmail.com>
2021-06-11 21:13:52 +0300

    second

--- asd.py
+++ asd.py
@@ -1,11 +1,32 @@
-# for i in range(5):
-#     if i !=0:
-#         print(end='\n')
-#     for j in range(8):
-#         print('A', end='')
-for i in range(13):
-    print()
+import random
 
-# hellou
+countries = {'Абхазия': 'Сухум',
+             'Австралия': 'Канберра',
+             'Австрия': 'Вена',
+             'Азербайджан': 'Баку',
+             'Албания': 'Тирана',
+             'Алжир': 'Алжир',
+             }
 
-#asd
+s = ['A', 'B', 'C', 'D']
+ck = [k for k, v in countries.items()]
+cv = [v for k, v in countries.items()]
+random.shuffle(ck)
+
+for count in range(1, 6):
+    fquest = open(f'Билет {count}', 'w')
+    fanswer = open(f'Ответ для билета {count}', 'w')
+    for i, k in enumerate(ck, 1):
+        rs = random.shuffle(cv)
+        fquest.write(f'{i}. Выберите столицу {k}\n')
+        answer = countries[k]
+        list_coustion = cv[:3]
+        list_coustion.append(answer)
+        random.shuffle(list_coustion)
+        for j in range(0, 4):
+            fquest.write(f'\t{s[j]}. {list_coustion[j]}\n')
+            if countries[k] == list_coustion[j]:
+                fanswer.write(f'{str(i)} {s[j]}\n')
+                break
+    fanswer.close()
+    fquest.close()
